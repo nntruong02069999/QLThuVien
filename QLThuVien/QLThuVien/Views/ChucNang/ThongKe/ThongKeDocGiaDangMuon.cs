554c7c4a1443fa4d281fd311975c81a34f567f11
thanhtung0310 <60694740+thanhtung0310@users.noreply.github.com>
2020-05-31 14:45:32 +0700

    chức năng Thống Kê "Những độc giả đang mượn sách"
    lỗi references -> không chạy được giao diện
    => đã sữa

--- QLThuVien/QLThuVien/Views/ChucNang/ThongKe/ThongKeDocGiaDangMuon.cs
+++ QLThuVien/QLThuVien/Views/ChucNang/ThongKe/ThongKeDocGiaDangMuon.cs
@@ -16,15 +16,37 @@ namespace QLThuVien.Views.ChucNang.ThongKe
 {
     public partial class ThongKeDocGiaDangMuon : DevExpress.XtraEditors.XtraUserControl
     {
-        Database db = new Database();
+        Database database = new Database();
+
         public ThongKeDocGiaDangMuon()
         {
             InitializeComponent();
         }
+
         public static ThongKeDocGiaDangMuon utcThongKeDocGiaDangMuon = new ThongKeDocGiaDangMuon();
+        
         private void ThongKeDocGiaDangMuon_Load(object sender, EventArgs e)
         {
-            
+            CommonDataSet commonDataSet = new CommonDataSet();
+            string check = "select dg.SoThe So_The, dg.TenDocGia Ten_Doc_Gia, ctt.SoCaBiet So_Ca_Biet ,ds.TenSach Ten_Sach , pm.NgayMuon Ngay_Muon , ctt.NgayTra Ngay_Tra " +
+                "FROM DocGia dg " +
+                "join TheThuVien ttv on ttv.ID_The = dg.SoThe " +
+                "join PhieuMuon pm on pm.SoThe = ttv.ID_The " +
+                "join ChungThucTra ctt on ctt.IdPhieuMuon = pm.IdPhieuMuon  " +
+                "join Sach s on s.SoCaBiet = ctt.SoCaBiet " +
+                "join DauSach ds on ds.MaSach = s.MaSach";
+            string strConnString = @"Data Source = DESKTOP-2NJ6H39\MSSQLSERVER1;Initial Catalog=QuanLyThuVien;Integrated Security=True";
+
+            SqlConnection cn = new SqlConnection(strConnString);
+            SqlDataAdapter da = new SqlDataAdapter(check, cn);
+
+            da.Fill(commonDataSet, commonDataSet.Tables[0].TableName);
+            ReportDataSource rds = new ReportDataSource("ThongKeDocGiaDangMuon", commonDataSet.Tables[0]);
+
+            this.rpLapPhieuMuon.LocalReport.DataSources.Clear();
+            this.rpLapPhieuMuon.LocalReport.DataSources.Add(rds);
+            this.rpLapPhieuMuon.LocalReport.Refresh();
+            this.rpLapPhieuMuon.RefreshReport();
         }
     }
 }
