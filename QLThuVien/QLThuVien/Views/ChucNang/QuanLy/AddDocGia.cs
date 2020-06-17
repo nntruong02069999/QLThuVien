5cbbfa1333ca0b6df7394afafe8193df97522436
viethoang99 <viet.mta.hp@gmail.com>
2020-06-17 22:05:28 +0700

    xong phần dơn giá

--- QLThuVien/QLThuVien/Views/ChucNang/QuanLy/AddDocGia.cs
+++ QLThuVien/QLThuVien/Views/ChucNang/QuanLy/AddDocGia.cs
@@ -22,7 +22,51 @@ namespace QLThuVien.Views.ChucNang.QuanLy
         }
         private void btLuu_Click(object sender, EventArgs e)
         {
-            
+            string soThe = tbSoThe.Text;
+            string tenDocGia = tbTenDocGia.Text;
+            string diaChi = tbSDT.Text;
+            string sdt = tbSDT.Text;
+            string nhanVienTao = cbNhanVienTao.Text;
+            DateTime ngayTao = dtpNgayTao.Value;
+            DateTime ngayHetHan = dtpNgayHetHan.Value;
+            if (string.IsNullOrWhiteSpace(soThe) || string.IsNullOrWhiteSpace(tenDocGia)
+                || string.IsNullOrWhiteSpace(diaChi) || string.IsNullOrWhiteSpace(sdt) ||
+                string.IsNullOrWhiteSpace(nhanVienTao))
+            {
+                XtraMessageBox.Show("Không được phép để trống thông tin độc giả !!", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (DateTime.Compare(ngayTao, ngayHetHan) >= 1)
+            {
+                XtraMessageBox.Show("Ngày hết hạn không thể có giá trị thấp hơn ngày khởi tạo !!", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string queryCheck = "Select * from TheThuVien where ID_The = N'" + soThe + "'";
+            DataTable data = new DataTable();
+            db.readDatathroughAdapter(queryCheck, data);
+            if (data.Rows.Count > 0)
+            {
+                XtraMessageBox.Show("Số thẻ đã tồn tại !!", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string getIDNhanVien = "Select * from NhanVien where TenNhanVien = N'" + cbNhanVienTao.Text + "'";
+            DataTable dt1 = new DataTable();
+            db.readDatathroughAdapter(getIDNhanVien, dt1);
+            string IdNhanVien = dt1.Rows[0].Field<string>("IDNhanVien");
+            //Insert TheThuVien
+            string queryInsertTheThuVien = "Insert into TheThuVien values (N'" + soThe + "', " +
+                "'" + ngayTao + "', '" + ngayHetHan + "', N'" + IdNhanVien + "' )";
+            SqlCommand sql1 = new SqlCommand(queryInsertTheThuVien);
+            db.executeQuery(sql1);
+            // Insert Độc giả
+            string queryInsertDocGia = "Insert into DocGia (DiaChi,TenDocGia,SDT,SoThe) " +
+                "values (N'" + diaChi + "', N'" + tenDocGia + "', N'" + sdt + "', N'" + soThe + "')";
+            SqlCommand sql2 = new SqlCommand(queryInsertDocGia);
+            db.executeQuery(sql2);
+
+            XtraMessageBox.Show("Thành công . ", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            QuanLyDocGia.utcQuanLyDocGia.QuanLyDocGiaLoading();
+            this.Hide();
         }
 
         private void AddDocGia_Load(object sender, EventArgs e)
@@ -31,7 +75,13 @@ namespace QLThuVien.Views.ChucNang.QuanLy
         }
         public void LoadingDocGia()
         {
-           
+            string queryNhanVien = "Select TenNhanVien From NhanVien";
+            DataTable nhanVien = new DataTable();
+            db.readDatathroughAdapter(queryNhanVien, nhanVien);
+            foreach (DataRow row in nhanVien.Rows)
+            {
+                cbNhanVienTao.Items.Add(row[0]);
+            }
         }
     }
 }
