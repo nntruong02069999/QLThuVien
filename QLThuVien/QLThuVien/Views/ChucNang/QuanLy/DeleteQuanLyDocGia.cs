0672e80ed830fa575db2977deb2b7b86e161e482
viethoang99 <viet.mta.hp@gmail.com>
2020-06-18 10:44:49 +0700

    xong phần xóa độc giả

--- QLThuVien/QLThuVien/Views/ChucNang/QuanLy/DeleteQuanLyDocGia.cs
+++ QLThuVien/QLThuVien/Views/ChucNang/QuanLy/DeleteQuanLyDocGia.cs
@@ -23,17 +23,58 @@ namespace QLThuVien.Views.ChucNang.QuanLy
 
         private void DeleteQuanLyDocGia_Load(object sender, EventArgs e)
         {
-            
+            string query = "Select * from TheThuVien";
+            DataTable dataTable = new DataTable();
+            db.readDatathroughAdapter(query, dataTable);
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                cbNhanVienTao.Items.Add(dataTable.Rows[i].Field<string>("ID_The"));
+            }
         }
 
         private void cbNhanVienTao_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            string soThe = cbNhanVienTao.Text;
+            string query = "Select TenDocGia , DiaChi , SDT , ttv.NgayHetHan " +
+                "From DocGia join TheThuVien ttv on DocGia.SoThe = ttv.ID_The " +
+                "where DocGia.SoThe = N'" + soThe + "'";
+            DataTable dataTable = new DataTable();
+            db.readDatathroughAdapter(query, dataTable);
+            tbDiaChi.Text = dataTable.Rows[0].Field<string>("DiaChi");
+            tbTenDocGia.Text = dataTable.Rows[0].Field<string>("TenDocGia");
+            tbSDT.Text = dataTable.Rows[0].Field<string>("SDT");
+            dtpNgayHetHan.Value = (DateTime)dataTable.Rows[0].Field<DateTime>("NgayHetHan");
         }
 
         private void btXoa_Click(object sender, EventArgs e)
         {
-            
+            string soThe = cbNhanVienTao.Text;
+            if (string.IsNullOrWhiteSpace(soThe))
+            {
+                XtraMessageBox.Show("Hãy chọn độc giả muốn xóa !!", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            DialogResult log = XtraMessageBox.Show("Bạn có chắc chắn muốn xóa ?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (log == DialogResult.Yes)
+            {
+                try
+                {
+                    string queryDeleteTTV = "Delete from TheThuVien where ID_The =N'" + soThe + "'";
+                    string queryDeleteDocGia = "Delete from DocGia where SoThe = N'" + soThe + "'";
+                    SqlCommand sql1 = new SqlCommand(queryDeleteTTV);
+                    SqlCommand sql2 = new SqlCommand(queryDeleteDocGia);
+
+                    db.executeQuery(sql2);
+                    db.executeQuery(sql1);
+                    XtraMessageBox.Show("Thành công .", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    QuanLyDocGia.utcQuanLyDocGia.QuanLyDocGiaLoading();
+                    this.Hide();
+                }
+                catch (Exception err)
+                {
+                    XtraMessageBox.Show("Độc giả không được phép xóa !!", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
         }
     }
 }
