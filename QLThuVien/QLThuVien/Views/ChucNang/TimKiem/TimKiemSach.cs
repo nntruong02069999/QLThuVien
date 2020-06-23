f0e11cf0953e2307bef1e288e9fc612a68a4a834
trananh1349 <anhtt@media-one.vn>
2020-06-23 14:31:30 +0700

    Hoàn thành chức năng tìm kiếm sách

--- QLThuVien/QLThuVien/Views/ChucNang/TimKiem/TimKiemSach.cs
+++ QLThuVien/QLThuVien/Views/ChucNang/TimKiem/TimKiemSach.cs
@@ -24,12 +24,24 @@ namespace QLThuVien.Views.ChucNang.TimKiem
         DataTable data = new DataTable();
         private void TimKiemSach_Load(object sender, EventArgs e)
         {
+            string querySachTrongThuVien = "SELECT MaSach as [Mã sách], TenSach as [Tên sách], tg.TenTacGia as [Tên tác giả], " +
+                "tl.TenTheLoai as [Tên thể loại], nxb.TenNXB as [Nhà xuất bản], NamXuatBan as [Năm xuất bản] " +
+                "From DauSach JOIN TacGia tg on DauSach.MaTacGia = tg.MaTacGia " +
+                "Join TheLoai tl on DauSach.MaTheLoai = tl.MaTheLoai " +
+                "join NXB nxb on DauSach.MaNXB = nxb.MaNXB";
             
+            db.readDatathroughAdapter(querySachTrongThuVien, data);
+            dgvTimKiemSach.DataSource = data;
         }
 
         private void tbTimKiem_KeyPress(object sender, KeyPressEventArgs e)
         {
-            
+            if(e.KeyChar == (char) 13)
+            {
+                DataView dv = data.DefaultView;
+                dv.RowFilter = string.Format("[Tên sách] like '%{0}%'", tbTimKiem.Text);
+                dgvTimKiemSach.DataSource = dv.ToTable();
+            }
         }
     }
 }
